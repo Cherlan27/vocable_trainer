87f9954d3993dd2b0f3848047813383217c41740
Lukas Petersdorf <l.petersdorf@gmail.com>
2025-12-07 14:01:37 +0100

    fix: remove async for test function

--- backend/test/api/router/test_get_vocables.py
+++ backend/test/api/router/test_get_vocables.py
@@ -1,10 +1,8 @@
-import pytest
 from fastapi.testclient import TestClient
 from src.api.app import app
 
 
-@pytest.mark.asyncio
-async def test_get_vocables():
+def test_get_vocables():
     client = TestClient(app, base_url="http://test")
     response = client.get("/api/cards")
     assert response.status_code == 200
